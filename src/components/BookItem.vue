60ab0813764dc3c566699055486c1b0091ecc91f
qianrenni <15133897+Qianrenni@user.noreply.gitee.com>
2025-09-17 21:58:42 +0800

    feat:首页

---
+++ src/components/BookItem.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import type {Book} from "../types";
+
+defineOptions({
+  name: 'BookItem'
+})
+defineProps<{
+  book: Book
+}>()
+</script>
+
+<template>
+  <div class="container-column book-meta" >
+    <h4 class="text-one-line">{{book.name}}</h4>
+    <h5 class="text-one-line">{{book.author}}</h5>
+    <span class="text-one-line tag">{{book.price}}￥</span>
+  </div>
+</template>
+
+<style scoped lang="css">
+
+</style>
